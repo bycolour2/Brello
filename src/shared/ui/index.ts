e9ffa01e0acc6f6ad833d8751cd1519ad01a04d0
bycolour2 <ruskir7@gmail.com>
2024-01-10 22:22:45 +0300

    Revert "BRE-39: setup basic cicd"

--- src/shared/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { Input } from "./Input";
-export { Button } from "./Button";
-export * from "./icons";
-export * from "./svgs";
--- src/shared/ui/svgs/index.ts
+++
@@ -1,3 +0,0 @@
-export { GeometricShapesDesktop } from "./GeometricShapesDesktop";
-export { GeometricShapesMobile } from "./GeometricShapesMobile";
-export { Logotype } from "./Logotype";

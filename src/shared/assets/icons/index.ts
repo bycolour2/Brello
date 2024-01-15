c5e03aaca0ad097489a1c4b1e1ef1e0d65ce638e
bycolour2 <ruskir7@gmail.com>
2024-01-15 16:16:24 +0300

    ✨ feat: update icons to work correct with tailwind

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -1,4 +1,6 @@
 import IconMail01 from "./mail-01.svg?react";
 import IconGoogleSocial from "./google-social-icon.svg?react";
+import IconArrowLeft from "./arrow-left.svg?react";
+import IconAlertCircle from "./alert-circle.svg?react";
 
-export { IconMail01, IconGoogleSocial };
+export { IconMail01, IconGoogleSocial, IconArrowLeft, IconAlertCircle };

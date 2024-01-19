f16c780d7940f8047b51147af5270db856f8bf14
bycolour2 <ruskir7@gmail.com>
2024-01-19 15:08:55 +0300

    ♻️ refactor: add layout components

--- src/pages/auth/sign-in/ui/Page.tsx
+++ src/pages/auth/sign-in/ui/Page.tsx
@@ -1,12 +1,14 @@
 import { ReactNode } from "react";
 import { useUnit } from "effector-react";
 
+import { LayoutAuthn } from "~/layouts/authn";
+
 import {
   IconAlertCircle,
   IconArrowLeft,
   IconMail01,
 } from "~/shared/assets/icons";
-import { Button, FeaturedIcon, Input, Logo, Spinner } from "~/shared/ui";
+import { Button, FeaturedIcon, Input, Spinner } from "~/shared/ui";
 
 import {
   $email,
@@ -23,26 +25,7 @@ import {
 export const SignInPage = () => {
   const [sended] = useUnit([$formSended]);
 
-  return (
-    <main className="relative flex h-screen flex-col items-center gap-8 pb-12 lg:h-screen lg:w-screen lg:flex-row lg:justify-between lg:gap-0 lg:p-0">
-      <div className="-order-1 h-16 w-full bg-[url('/src/pages/auth/SignIn/ui/geometric-shapes-mobile.svg')] bg-center lg:order-1 lg:aspect-[4/5] lg:h-screen lg:w-auto lg:bg-[url('/src/pages/auth/SignIn/ui/geometric-shapes-desktop.svg')] lg:bg-cover" />
-      <div className="flex flex-col items-center justify-between gap-8 self-stretch px-4 pt-24 lg:min-w-[560px] lg:flex-grow lg:pt-0">
-        <header className="hidden flex-col items-start gap-6 self-stretch lg:flex lg:h-24 lg:p-8">
-          <Logo />
-        </header>
-        <section className="flex flex-col items-center gap-8 self-stretch lg:min-w-[360px] lg:self-auto lg:px-8">
-          {!sended ? <LoginForm /> : <LoginResult />}
-        </section>
-        <footer className="hidden lg:flex lg:h-24 lg:items-end lg:justify-between lg:self-stretch lg:p-9">
-          <p className="text-sm text-gray-600">© Brello 2023</p>
-          <p className="flex items-center gap-2 text-sm text-gray-600">
-            <IconMail01 className="h-4 w-4 stroke-gray-600" />
-            help@brello.io
-          </p>
-        </footer>
-      </div>
-    </main>
-  );
+  return <LayoutAuthn>{!sended ? <LoginForm /> : <LoginResult />}</LayoutAuthn>;
 };
 
 //NOTE - good practice
